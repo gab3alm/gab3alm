1ce4ce83baba6dbd3c4580f0d5a607f8111e6c81
gab3alm <gab.alm94@gmail.com>
2018-08-21 22:21:30 -0700

    Styles for navigation bar

--- src/gab3alm/components/navbar.cljs
+++ src/gab3alm/components/navbar.cljs
@@ -2,14 +2,10 @@
 
 (defn navigation-bar []
   [:nav {:class "navbar navbar-expand-lg"}
-   ;button toggler
-   [:button {:class "navbar-toggler" :type "button" :data-toggle "collapse" :data-target "#navbarItems" :aria-controls "navbarItems" :aria-expanded "false" :aria-label "Toggle Navigation"}
-    [:span {:class "navbar-toggler-icon"}]]
-
    ;navbar items
-   [:div {:class "collapse navbar-collapse" :id "navbarItems"}
-    [:ul {:class "navbar-nav ml-auto"}
-     [:li {:class "nav-item"} [:a {:href "" :class "nav-link"} "journal"]]
-     [:li {:class "nav-item"} [:a {:href "" :class "nav-link"} "projects"]]
-     [:li {:class "nav-item"} [:a {:href "" :class "nav-link"} "resume"]]
-     ]]])
+   [:ul {:class "navbar-nav ml-auto"}
+    [:li {:class "nav-item"} [:a {:href "" :class "nav-link"} "journal"]]
+    [:li {:class "nav-item"} [:a {:href "" :class "nav-link"} "projects"]]
+    [:li {:class "nav-item"} [:a {:href "" :class "nav-link"} "resume"]]]
+   [:a {:class "navbar-brand" :href "#"} [:img {:src "/images/logo.svg"}]]
+   ])
